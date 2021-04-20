dbf4995a0569fd20ff748f7caef441e4b3efd1cf
gahee <rkgml284@gmail.com>
2021-04-20 18:19:48 +0900

    JDBC 연결테스트

--- src/gahee/basic/day16/HelloJDBC.java
+++ src/gahee/basic/day16/HelloJDBC.java
@@ -20,7 +20,7 @@ public class HelloJDBC {
 
         // amazon 데이터베이스 접속 확인
         String drv = "org.mariadb.jdbc.Driver";
-        String amz = "bigdata.cfss7vpoctox.ap-northeast-2.rds.amazonaws.com";
+        String amz = "";
         String url = "jdbc:mariadb://"+amz+":3306/bigdata";
         String usr = "bigdata";
         String pwd = "bigdata2020";
